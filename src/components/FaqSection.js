168e1081c36538e00df64eda04c5ae729e8ba68e
shuvasish <shuvasishtalukdershuvoalways@gmail.com>
2021-04-05 22:08:13 +0600

    faq section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+function FaqSection(props) {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do i start up?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+            autem accusamus ex laboriosam porro, adipisci quam voluptatum magnam
+            placeat corporis.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+            autem accusamus ex laboriosam porro, adipisci quam voluptatum magnam
+            placeat corporis.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Diferrent Payment Methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+            autem accusamus ex laboriosam porro, adipisci quam voluptatum magnam
+            placeat corporis.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Products do you offer.</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+            autem accusamus ex laboriosam porro, adipisci quam voluptatum magnam
+            placeat corporis.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,6 +1,7 @@
 import React from "react";
 //import components
 import AboutSection from "../components/AboutSection";
+import FaqSection from "../components/FaqSection";
 import ServicesSection from "../components/ServicesSection";
 
 function AboutUs(props) {
@@ -8,6 +9,7 @@ function AboutUs(props) {
     <>
       <AboutSection></AboutSection>
       <ServicesSection></ServicesSection>
+      <FaqSection></FaqSection>
     </>
   );
 }
