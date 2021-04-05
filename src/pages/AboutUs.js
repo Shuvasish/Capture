87c89b84c6241570599750b2e169a1eb1574b197
shuvasish <shuvasishtalukdershuvoalways@gmail.com>
2021-04-05 21:39:14 +0600

    about section

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,13 @@
+import React from "react";
+//import components
+import AboutSection from "../components/AboutSection";
+
+function AboutUs(props) {
+  return (
+    <div>
+      <AboutSection></AboutSection>
+    </div>
+  );
+}
+
+export default AboutUs;
