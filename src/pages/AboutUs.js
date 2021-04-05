a1284eb568c1d88f92a4adbf41057b52ca7c10f4
shuvasish <shuvasishtalukdershuvoalways@gmail.com>
2021-04-06 01:38:32 +0600

    added navbar

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,11 +2,13 @@ import React from "react";
 //import components
 import AboutSection from "../components/AboutSection";
 import FaqSection from "../components/FaqSection";
+import Nav from "../components/Nav";
 import ServicesSection from "../components/ServicesSection";
 
 function AboutUs(props) {
   return (
     <>
+      <Nav></Nav>
       <AboutSection></AboutSection>
       <ServicesSection></ServicesSection>
       <FaqSection></FaqSection>
