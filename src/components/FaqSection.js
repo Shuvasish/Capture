bd7392b2bbf9bcf7fa2763ec83e1f1cdfa951772
shuvasish <shuvasishtalukdershuvoalways@gmail.com>
2021-04-06 23:18:47 +0600

    added toggle component

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import { About } from "../styles.js";
+import Toggle from "./Toggle";
+import { AnimateSharedLayout } from "framer-motion";
 
 function FaqSection(props) {
   return (
@@ -8,54 +10,50 @@ function FaqSection(props) {
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <div className="question">
-        <h4>How do i start up?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
-            autem accusamus ex laboriosam porro, adipisci quam voluptatum magnam
-            placeat corporis.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
-            autem accusamus ex laboriosam porro, adipisci quam voluptatum magnam
-            placeat corporis.
-          </p>
-        </div>
-      </div>
-      <div className="faq-line"></div>
-      <div className="question">
-        <h4>Diferrent Payment Methods</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
-            autem accusamus ex laboriosam porro, adipisci quam voluptatum magnam
-            placeat corporis.
-          </p>
-        </div>
-      </div>
-      <div className="faq-line"></div>
-      <div className="question">
-        <h4>What Products do you offer.</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
-            autem accusamus ex laboriosam porro, adipisci quam voluptatum magnam
-            placeat corporis.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+      <AnimateSharedLayout>
+        <Toggle title="How do i start up?">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+              autem accusamus ex laboriosam porro, adipisci quam voluptatum
+              magnam placeat corporis.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Daily Schedule">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+              autem accusamus ex laboriosam porro, adipisci quam voluptatum
+              magnam placeat corporis.
+            </p>
+          </div>
+        </Toggle>
+
+        <Toggle title="Diferrent Payment Methods">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+              autem accusamus ex laboriosam porro, adipisci quam voluptatum
+              magnam placeat corporis.
+            </p>
+          </div>
+        </Toggle>
+
+        <Toggle title="What Products do you offer.">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+              autem accusamus ex laboriosam porro, adipisci quam voluptatum
+              magnam placeat corporis.
+            </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
     </Faq>
   );
 }
